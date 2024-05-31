9ddd4e967b2177da33836e0a06417ec2e7bce40f
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2024-05-31 11:45:27 +0100

    terraform init

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+
+  cloud {
+    organization = "enbloc"
+    workspaces {
+      name = "terraform-cutie"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
